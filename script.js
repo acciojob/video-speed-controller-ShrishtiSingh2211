cc7c75eb8960d365e323e57894051d40775ba952
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-04-03 12:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.player__video');
+const toggleBtn = document.querySelector('.toggle');
+const skipBtns = document.querySelectorAll('[data-skip]');
+const sliders = document.querySelectorAll('.player__slider');
+const progress = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+    toggleBtn.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    toggleBtn.textContent = '►';
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+toggleBtn.addEventListener('click', togglePlay);
+skipBtns.forEach(btn => btn.addEventListener('click', skip));
+sliders.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
